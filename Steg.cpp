c8f45e70f161cbbf4fb390d5b6a5c440abd33a25
Cole Davis <coledavis6499@gmail.com>
2018-05-15 23:16:41 -0500

    Added Hello World

--- Steg.cpp
+++ Steg.cpp
@@ -5,3 +5,12 @@
   Goal: Get started on Steg, learn 
   how to access the bits of an image
 =====================================*/
+
+#include <iostream>
+
+
+int main()
+{
+  std::cout<<"Hello world!"<<endl;
+  return 0;
+}
