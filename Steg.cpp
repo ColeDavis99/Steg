be170c4221d6d8ead716eccec56f89b52194d02f
Cole Davis <coledavis6499@gmail.com>
2018-05-16 13:28:02 -0500

    Succesful 'dumb toggle' of LSB

--- Steg.cpp
+++ Steg.cpp
@@ -2,26 +2,73 @@
   Developer: Cole Davis
   Date: 05-15-2018
 
-  Goal: Get started on Steg, learn 
-  how to access the bits of an image
+  Goal: Manipulate the RGB values of
+  two images and toggle the LSB
 =====================================*/
+
+#include <iostream>
+#include <bitset>
+using std::cout;
+using std::endl;
+using std::bitset;
 
 #include "CImg.h"
 using namespace cimg_library;
 
-int main() 
+int main()
 {
-  // Define a 640x400 color image with 8 bits per color component.
-	CImg<unsigned char> img(640,400,1,3); 
-	img.fill(0);
-	
-	// Set pixel values to 0 (color : black)
-	unsigned char purple[] = { 255,0,255 };
-	
-  // Define a purple color
-  img.draw_text(100,100,"Hello World",purple); // Draw a purple "Hello world" at coordinates (100,100).
-	
-  // Display the image in a display window.
-  img.display("My first CImg code");
+  //Make image and viewing window
+  CImg<unsigned char> image("pic2.png");
+  CImgDisplay disp1(image,"Before!");
+  CImg<unsigned char> image2 = image;
+  
+
+ 
+  
+
+  //Prints out 0-255 int of every R, every G, then every B
+  cimg_for(image2,ptr,unsigned char)
+  {
+    cout<<(int)*ptr<<" ";
+  }
+  
+  
+  
+  //This loop toggles the last bit
+  cimg_for(image2,ptr,unsigned char)
+  {
+    if(stoi(bitset<8>(*ptr).to_string()) %2 == 1)
+      *ptr = *ptr - 1;
+
+    else
+      *ptr = *ptr + 1;
+  }
+  
+  
+  
+  cout<<endl<<endl<<endl;
+  //This loop prints out the new toggled RGB value
+  cimg_for(image2,ptr,unsigned char)
+  {
+    cout<<(int)*ptr<<" ";
+  }
+  
+  
+  
+  
+  
+  
+  //Show the before and after
+  CImgDisplay disp2(image2,"After!");
+  while (!disp1.is_closed() && !disp2.is_closed())
+  {
+      disp1.wait();       
+      disp2.wait();       
+      image.display(disp1);
+      image2.display(disp2);
+  }
+  
+  
+  
   return 0;
 }
