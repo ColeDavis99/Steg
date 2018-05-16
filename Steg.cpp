12bca084f6bd4bd86722ad6fbb8a7ca2a4e53c73
Cole Davis <coledavis6499@gmail.com>
2018-05-15 21:12:09 -0500

    First Commit

---
+++ Steg.cpp
@@ -0,0 +1,7 @@
+/*====================================
+  Developer: Cole Davis
+  Date: 05-15-2018
+
+  Goal: Get started on Steg, learn 
+  how to access the bits of an image
+=====================================*/
