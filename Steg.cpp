b927ee58bdc0df500263d56d8a3e37ce5d23d8ba
Cole <coledavis6499@gmail.com>
2018-05-16 09:30:55 -0500

    Update Steg.cpp

--- Steg.cpp
+++ Steg.cpp
@@ -12,11 +12,11 @@ using namespace cimg_library;
 int main() 
 {
   // Define a 640x400 color image with 8 bits per color component.
-	CImg<unsigned char> img(640,400,1,3); 
-	img.fill(0);
+  CImg<unsigned char> img(640,400,1,3); 
+  img.fill(0);
 	
-	// Set pixel values to 0 (color : black)
-	unsigned char purple[] = { 255,0,255 };
+  // Set pixel values to 0 (color : black)
+  unsigned char purple[] = { 255,0,255 };
 	
   // Define a purple color
   img.draw_text(100,100,"Hello World",purple); // Draw a purple "Hello world" at coordinates (100,100).
