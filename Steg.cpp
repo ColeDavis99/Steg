204956cd25560be9bf8fb10ead9bee5908582414
Cole Davis <coledavis6499@gmail.com>
2018-05-16 08:45:10 -0500

    Added CImg equivalent of 'hello world'

--- Steg.cpp
+++ Steg.cpp
@@ -6,11 +6,22 @@
   how to access the bits of an image
 =====================================*/
 
-#include <iostream>
+#include "CImg.h"
+using namespace cimg_library;
 
-
-int main()
+int main() 
 {
-  std::cout<<"Hello world!"<<endl;
+  // Define a 640x400 color image with 8 bits per color component.
+	CImg<unsigned char> img(640,400,1,3); 
+	img.fill(0);
+	
+	// Set pixel values to 0 (color : black)
+	unsigned char purple[] = { 255,0,255 };
+	
+  // Define a purple color
+  img.draw_text(100,100,"Hello World",purple); // Draw a purple "Hello world" at coordinates (100,100).
+	
+  // Display the image in a display window.
+  img.display("My first CImg code");
   return 0;
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+default: Steg.cpp
+	g++ Steg.cpp -lpthread -lX11 -o exe -std=c++11
